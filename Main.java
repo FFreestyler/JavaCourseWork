0be53c44f3bf34ebba04dc89d92c94c7e31aa6f5
Menchshikov Danil <freeestyler231@gmail.com>
2022-05-13 16:33:34 +0700

    Implemented CPU Usage score

--- Main.java
+++ Main.java
@@ -1,5 +1,43 @@
+import static java.lang.System.out;
+
+import java.io.*;
+import java.net.*;
+import java.security.cert.LDAPCertStoreParameters;
+import java.util.*;
+
 class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+  public static void main(String args[])
+    throws IOException, InterruptedException {
+    parseData();
+  }
+
+  public static void parseData() throws IOException, InterruptedException {
+    ProcessBuilder pb = new ProcessBuilder(
+      "ps",
+      "-eo",
+      "%cpu",
+      "--sort",
+      "-%cpu"
+    );
+    pb.redirectError();
+    Process proc = pb.start();
+    BufferedReader bf = new BufferedReader(
+      new InputStreamReader(proc.getInputStream())
+    );
+    String s;
+    ArrayList<String> CPUList = new ArrayList<>();
+    Double num = .0;
+    while ((s = bf.readLine()) != null) {
+      if (s.contains("%CPU")) {
+        continue;
+      }
+      CPUList.add(s.replaceAll("\\s+", ""));
     }
+    for (String str : CPUList) {
+      //System.out.println(str);
+      num += Double.parseDouble(str);
+    }
+    System.out.println(num);
+  }
 }
